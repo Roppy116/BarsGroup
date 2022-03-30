81e25be9c1d13620c287c423865ebbcdade05043
Roppy116 <YusupovIlF@gmail.com>
2022-03-30 18:55:26 +0300

    NewCommit

---
+++ Collections/collections.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Collections
+{
+    class Entity
+    {
+        public int Id { get; set; }
+        public int ParentId { get; set; }
+        public string Name { get; set; }
+
+
+        public static Dictionary<int, List<Entity>> Method(List<Entity> list)
+        {
+            if (list.Count == 0)
+                throw new Exception("List is empty");
+            var dict = list.GroupBy(x => x.ParentId).ToDictionary(ParentId => ParentId.Key, x => x.ToList());
+
+            foreach (var item in dict)
+                Console.WriteLine($"Key = {item.Key}, Value = List {string.Join(" ", item.Value)}");
+
+            return dict;
+
+        }
+
+    }
+
+    class collections
+    {
+        static void Main(string[] args)
+        {
+
+            List<Entity> list = new List<Entity>
+            {
+                new Entity { Id= 1,  ParentId = 0,Name = "Root entity"},
+                new Entity { Id= 2,  ParentId = 1,Name = "Child of 1 entity" },
+                new Entity { Id= 3,  ParentId = 1,Name = "Child of 1 entity" },
+                new Entity { Id= 4,  ParentId = 2,Name = "Child of 2 entity" },
+                new Entity { Id= 5,  ParentId = 4,Name = "Child of 4 entity" }
+            };
+
+            Entity.Method(list);
+
+        }
+    }
+}
---
+++ Logger/logger.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+
+namespace Logger
+{
+    public interface ILogger
+    {
+        void LogInfo(string message);
+        void LogWarning(string message);
+        void LogError(string message, Exception ex);
+    }
+
+    class LocalFileLogger<T> : ILogger
+    {
+        private string path;
+        public LocalFileLogger(string path)
+        {
+            this.path = path;
+        }
+
+        public void LogInfo(string message)
+        {
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteLine($"[Info]: [{typeof(T).Name}] : {message}");
+            sw.Close();
+        }
+
+        public void LogWarning(string message)
+        {
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteLine($"[Warning] : [{typeof(T).Name}] : {message}");
+            sw.Close();
+        }
+
+        public void LogError(string message, Exception ex)
+        {
+            StreamWriter sw = new StreamWriter(path, true);
+            sw.WriteAsync($"[Error] : [{typeof(T).Name}] : {message}. {ex.Message}");
+            sw.Close();
+        }
+    
+    }
+
+    class logger
+    {
+        static void Main(string[] args)
+        {
+            LocalFileLogger<string> string_logger = new LocalFileLogger<string>("test123.txt");
+            string_logger.LogInfo("Some string Information");
+            string_logger.LogWarning("Some string Warning");
+            string_logger.LogError("Some string Error", new Exception("ERROR!"));
+
+            LocalFileLogger<int> int_logger = new LocalFileLogger<int>("test123.txt");
+            int_logger.LogInfo("Some Int Information");
+            int_logger.LogWarning("Some Int Warning");
+            int_logger.LogError("Some Int Error", new Exception("ERROR!"));
+        }
+    }
+}
